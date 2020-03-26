2fa56784f14553875c5e2ea16ae393be48f9a471
Ravi Teja Gudapati <tejainece@gmail.com>
2020-03-26 17:25:29 +0100

    player insert test

--- test/db/player/player_test.dart
+++ test/db/player/player_test.dart
@@ -1,13 +1,30 @@
 import 'package:libpg/libpg.dart';
 
 import 'package:test/test.dart';
+import 'package:universal_server/db/user.dart';
 
 void main() {
+  final db = PGPool(settings);
+  final userDb = UserDBImpl();
 
+  setUp(() async {
+    // TODO create database
+  });
+
+  tearDown(() async {
+    // TODO drop database
+    await db.close();
+  });
 
   group('db.player', () {
     test('insert', () async {
-
+      final user = await userDb.insert(db, 'tejainece', 'tejainece@gmail.com', 'testing');
+      expect(user.id, isNotNull);
+      expect(user.username, 'tejainece');
+      expect(user.email, 'tejainece@gmail.com');
+      expect(user.password, isNotNull); // TODO test hash
+      expect(user.joined, isNotNull);
+      expect(user.emailVerified, false);
     });
   });
 }
